a13413ac3e9ddebf15ad2dfed63b9fb154a97bed
vlamptey <vlamptey@gmail.com>
2023-07-12 14:57:49 -0400

    testing

--- vpc.tf
+++ vpc.tf
@@ -3,12 +3,30 @@ provider "aws" {
 }
 
 
-resource "aws_default_vpc" "defvpc02" {
-  // cidr_block = "172.31.0.0/16"
-  enable_dns_hostnames = true
-  force_destroy        = true
-  tags = {
-    Environment = "Prodvpc"
-    Name        = "default vpc"
+# resource "aws_default_vpc" "defvpc02" {
+#   // cidr_block = "172.31.0.0/16"
+#   enable_dns_hostnames = true
+#   force_destroy        = true
+#   tags = {
+#     Environment = "Prodvpc"
+#     Name        = "default vpc"
+#   }
+# }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
   }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
 }
