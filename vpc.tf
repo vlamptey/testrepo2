8b569b627ba29da0f4e53f2ca0cd8384cad3de59
vlamptey <vlamptey@gmail.com>
2023-07-12 21:12:07 -0400

    first commit

--- vpc.tf
+++ vpc.tf
@@ -1,28 +1,10 @@
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-west-2"
+
 }
-
-
-# resource "aws_default_vpc" "defvpc02" {
-#   // cidr_block = "172.31.0.0/16"
-#   enable_dns_hostnames = true
-#   force_destroy        = true
-#   tags = {
-#     Environment = "Prodvpc"
-#     Name        = "default vpc"
-#   }
-# }
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 4.0"
-#     }
-#   }
-# }
-
 # Configure the AWS Provider
 # Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "example2" {
+  cidr_block = "20.0.0.0/24"
+  
 }
