e0e4983c6f80484e1aa1c26822468c4de07df307
vlamptey <vlamptey@gmail.com>
2023-07-12 20:55:12 -0400

    first commit

--- vpc.tf
+++ vpc.tf
@@ -12,14 +12,14 @@ provider "aws" {
 #     Name        = "default vpc"
 #   }
 # }
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 4.0"
+#     }
+#   }
+# }
 
 # Configure the AWS Provider
 # Create a VPC
